57368b7e9fafc80bde645c8607286897a8d4a7b4
Brian Huntley <brian_huntley@my.uri.edu>
2020-04-30 15:43:47 -0400

    added button to generate array

--- site.js
+++ site.js
@@ -1,4 +1,11 @@
-function helloWorld(){
-    alert("Hello");
+function generateArray(){
+    return Array(15).fill().map(() => Math.round(Math.random() * 25));
+}
+
+function drawArray(arr){
+    for(let i = 0; i < arr.length; ++i){
+        rect(30 + 10*arr[i],50,15,10*arr[i]);
+        
+    }
 }
 
