d393a2fcccfdc331e5a41f8bc044e0fadd6283ca
Brian Huntley <brian_huntley@my.uri.edu>
2020-04-30 19:23:06 -0400

    Figured out how canvas works, added very basic temp drawing feature

--- site.js
+++ site.js
@@ -1,10 +1,13 @@
 function generateArray(){
-    return Array(15).fill().map(() => Math.round(Math.random() * 25));
+    let arr = Array(15).fill().map(() => Math.round(Math.random() * 25));
+    drawArray(arr);
+    return arr;
 }
 
 function drawArray(arr){
+    var c = canvas.getContext("2d");
     for(let i = 0; i < arr.length; ++i){
-        rect(30 + 10*arr[i],50,15,10*arr[i]);
+        c.fillRect(30 + 50*i,400,15,10*-arr[i]);
         
     }
 }
