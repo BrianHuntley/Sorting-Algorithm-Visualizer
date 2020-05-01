327222a2cfa38a6bf19cdcdac648fb24dd4d84c7
Brian Huntley <brian_huntley@my.uri.edu>
2020-05-01 12:56:25 -0400

    added numbers under graph, sorting now draws graph

--- site.js
+++ site.js
@@ -3,14 +3,3 @@ function generateArray(){
     drawArray(arr);
     return arr;
 }
-
-function drawArray(arr){
-    var c = canvas.getContext("2d");
-    //y axis
-    c.fillRect(100,400,1,-300);
-    //x axis
-    c.fillRect(100,400,750,1);
-    for(let i = 0; i < arr.length; ++i){
-        c.fillRect(100 + 50*i,400,15,10*-arr[i]);
-    }
-}
