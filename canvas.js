b6a25c361743499edf94fb938f18f2bef5d63877
Brian Huntley <brian_huntley@my.uri.edu>
2020-05-02 14:18:41 -0400

    bars are now rectangles so I can animate them later

--- canvas.js
+++ canvas.js
@@ -7,15 +7,14 @@ var c = canvas.getContext("2d");
 //x axis
 c.fillRect(100,400,750,1);
 
-function drawArray(arr){
+function drawArray(barArray){
     c.clearRect(0,0,window.innerWidth, window.innerHeight);
     //x axis
     c.fillRect(100,400,750,1);
 
-    for(let i = 0; i < arr.length; ++i){
-        c.fillRect(100 + 50*i,400,15,10*-arr[i]);
-        c.font = "30px Arial"
-        c.fillText(arr[i], 100 + 50*i, 430);
+    for(let i = 0; i < barArray.length; ++i){
+        barArray[i].draw();
+    
     }
 }
 
--- site.js
+++ site.js
@@ -1,5 +1,22 @@
+function Bar(val, pos){
+    this.val = val;
+    this.pos = pos;
+
+    this.draw = function(){
+        c.fillRect(100 + 50*this.pos,400,15,15*-this.val);
+        c.font = "30px Arial"
+        c.fillText(this.val, 100 + 50*this.pos, 430);
+    }
+}
+
 function generateArray(){
-    let arr = Array(15).fill().map(() => Math.round(Math.random() * 25));
-    drawArray(arr);
+    var barArray = [];
+    for(let i = 0; i < 15; ++i){
+        var val = Math.round(Math.random()* 24)+1;
+        console.log(val);
+        barArray.push(new Bar(val,i));
+    }
+    console.log(barArray);
+    drawArray(barArray);
     return arr;
 }
