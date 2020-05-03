765c2ed9e56b6e3b62e888ab875298d572bedf0b
Brian Huntley <brian_huntley@my.uri.edu>
2020-05-02 21:19:15 -0400

    centered the graph

--- canvas.js
+++ canvas.js
@@ -1,16 +1,18 @@
 var canvas = document.querySelector("canvas");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
 var c = canvas.getContext("2d");
-
+var centerX = (canvas.width/2)-375;
+var centerY = canvas.height/2;
 //x axis
-c.fillRect(100,400,750,1);
+c.fillRect(centerX, centerY, 750, 1);
 
 function drawArray(barArray){
     c.clearRect(0,0,window.innerWidth, window.innerHeight);
     //x axis
-    c.fillRect(100,400,750,1);
+    c.fillRect(centerX, centerY, 750, 1);
+
 
     for(let i = 0; i < barArray.length; ++i){
         barArray[i].draw();
--- site.js
+++ site.js
@@ -3,9 +3,9 @@ function Bar(val, pos){
     this.pos = pos;
 
     this.draw = function(){
-        c.fillRect(100 + 50*this.pos,400,15,15*-this.val);
+        c.fillRect(centerX + 50*this.pos, centerY, 15, 15*-this.val);
         c.font = "30px Arial"
-        c.fillText(this.val, 100 + 50*this.pos, 430);
+        c.fillText(this.val, centerX + 50*this.pos, centerY + 30);
     }
 }
 
